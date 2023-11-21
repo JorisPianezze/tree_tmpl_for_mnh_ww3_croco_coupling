7d835fa9ec1381443eaeb0df39af1d389e1d5da1
JorisP <pianezze.joris@gmail.com>
2023-11-21 14:50:51 +0100

    Add environment variables checks

--- 1_input_mnh/run_prep_pgd.sh
+++ 1_input_mnh/run_prep_pgd.sh
@@ -15,22 +15,44 @@ ulimit -s unlimited
 ulimit -c 0
 
 # --------------------------------------------------
-# -- load Meso-NH/SurfEx environment variables
+#    Load Meso-NH/SurfEx environment variables
 # --------------------------------------------------
 
-. ../../models/MNH-V5-5-0/conf/profile_mesonh
+. /path/to/your/profile_mesonh
+
+if [ -z ${XYZ} ] ; then
+   echo '      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~           '
+   echo ' XYZ is not define, please load profile_mesonh'
+   echo '      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~           '
+   exit
+fi
 
 # --------------------------------------------------
-# -- link to the data
+#    Link to the data and run PREP_PGD
 # --------------------------------------------------
 
-ln -sf ${PREP_PGD_FILES}/CLAY_HWSD_MOY.??? .
-ln -sf ${PREP_PGD_FILES}/SAND_HWSD_MOY.??? .
-ln -sf ${PREP_PGD_FILES}/gtopo30.??? .
-ln -sf ${PREP_PGD_FILES}/ECOCLIMAP_v2.0.??? .
+export PREP_PGD_FILES=${PREP_PGD_FILES:-"$HOME/PREP_PGD_FILES_WWW"}
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#--
-time mpirun -np 1 PREP_PGD${XYZ} | tee output_PREP_PGD.out
-#--
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if [ ! -d ${PREP_PGD_FILES} ]
+then
+
+   echo 'Your directory PREP_PGD_FILES=$PREP_PGD_FILES doesnt exist.'
+   echo 'Please define the location of your PREP_PGD_FILES through'
+   echo 'the environment variable PREP_PGD_FILES.'
+   exit
+
+else
+
+   ln -sf $PREP_PGD_FILES/CLAY_HWSD_MOY.??? .
+   ln -sf $PREP_PGD_FILES/SAND_HWSD_MOY.??? .
+   ln -sf $PREP_PGD_FILES/ECOCLIMAP_v2.0.??? .
+   ln -sf $PREP_PGD_FILES/gtopo30.??? .
+   ln -sf $PREP_PGD_FILES/etopo2.nc .
+
+   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   #--
+   time mpirun -np 1 PREP_PGD${XYZ} | tee output_PREP_PGD.out
+   #--
+   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+fi
